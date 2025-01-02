fecf74e0c59124e54aede4d2bd6a4bac43e3f3b4
Christian Diang <christiandiang2230@gmail.com>
2025-01-02 23:19:16 +0800

    try not using js rendering

--- scripts/brands.js
+++ scripts/brands.js
@@ -1,40 +1,40 @@
-const brands = [
-    {
-        brand: 'Hyundai',
-        image: '../images/hyundai.png'
-    },
-    {
-        brand: 'Mitsubishi',
-        image: '../images/mitsubishi.png'
-    },
-    {
-        brand: 'Nissan',
-        image: '../images/nissan.jpg'
-    },
-    {
-        brand: 'Toyota',
-        image: '../images/toyota.jpg'
-    },
-]
+// const brands = [
+//     {
+//         brand: 'Hyundai',
+//         image: '../images/hyundai.png'
+//     },
+//     {
+//         brand: 'Mitsubishi',
+//         image: '../images/mitsubishi.png'
+//     },
+//     {
+//         brand: 'Nissan',
+//         image: '../images/nissan.jpg'
+//     },
+//     {
+//         brand: 'Toyota',
+//         image: '../images/toyota.jpg'
+//     },
+// ]
 
 
-renderBrands()
+// renderBrands()
 
-export function renderBrands(){
-    let brandsHTML = ''
-    brands.forEach((brand)=>{
-        brandsHTML += 
-        `
-            <div class="brand">
-                <div class="image-container">
-                <img src=${brand.image} alt="">
-                </div>
-                <h3>${brand.brand}</h3>
-            </div>
-        `
+// export function renderBrands(){
+//     let brandsHTML = ''
+//     brands.forEach((brand)=>{
+//         brandsHTML += 
+//         `
+//             <div class="brand">
+//                 <div class="image-container">
+//                 <img src=${brand.image} alt="">
+//                 </div>
+//                 <h3>${brand.brand}</h3>
+//             </div>
+//         `
     
-        document.querySelector('.cars-container').innerHTML = brandsHTML
-    })
-}
+//         document.querySelector('.cars-container').innerHTML = brandsHTML
+//     })
+// }
 
-console.log("Hello world")
+// console.log("Hello world")
